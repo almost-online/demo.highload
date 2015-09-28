2cdf470da7e3c52fc76386256c3482f03781ee35
almost-online <almost@online.com.ua>
2015-09-28 18:01:03 +0300

    change project to rest edition

--- src/API/UserBundle/Controller/DefaultController.php
+++ src/API/UserBundle/Controller/DefaultController.php
@@ -2,54 +2,20 @@
 
 namespace API\UserBundle\Controller;
 
-
-use FOS\RestBundle\Context\Context;
-//use FOS\RestBundle\Controller\FOSRestController;
-use API\UserBundle\Entity\User;
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
 
-class DefaultController
+
+class DefaultController extends FOSRestController
 {
 
     public function indexAction()
     {
-        $view = View::create();
+        $data = array('date' => date('r'));
 
-        $view->setData(array('date' => date('r'), 'name' => 'some name'));
+        $view = $this->view($data, 200);
         $view->setTemplate('APIUserBundle:Default:index.html.twig')
             ->setTemplateData(array('name' => 'some name'));
 
-        return $view;
+        return $this->handleView($view);
     }
-
-//    public function allAction()
-//    {
-//
-//        $em = $this->getDoctrine()->getManager();
-//
-//        $users = $em->getRepository('APIUserBundle:User')->findAll();
-//
-//        $view = $this->view($users, 200)
-//            ->setTemplate("APIUserBundle:Default:getUsers.html.twig")
-//            ->setTemplateVar('users')
-//        ;
-//
-//        return $this->handleView($view);
-//
-//    }
-//
-//    public function getAction($id)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//        $user = $em->getRepository('APIUserBundle:User')->findPk($id);
-//
-//        if (!$user instanceof User) {
-//            throw new NotFoundHttpException('User not found');
-//        }
-//
-//        return array('user' => $user);
-//    }
 }
